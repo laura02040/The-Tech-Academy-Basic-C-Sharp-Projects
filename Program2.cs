2e75bcae53e9e4b3ea1ea1fc8c1d636605534258
laura02040 <131707496+laura02040@users.noreply.github.com>
2025-11-16 20:17:56 +0000

    Do while loop
    do while loop

---
+++ Program2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is do while loop
+            int count = 0;
+            do
+            {
+                Console.WriteLine("Inside the loop");
+                count++; //add one to count each time
+            }
+            while (count < 5); //continue to loop until count reaches 5
+            //this is while loop
+            int counting = 0;
+            while (counting < 3) //continue to loo[p until counting reach 3
+            {
+                Console.WriteLine("Counting numbers");
+                counting++; //add one to countingf each time
+            }
+            Console.ReadLine();
+        }
+    }
+}
