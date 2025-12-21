2c715504e35c0bb259b9c9e169e44c6db35bfcd1
laura02040 <131707496+laura02040@users.noreply.github.com>
2025-12-21 21:54:56 +0000

    Main Method 2
    Main Method 2

---
+++ Main Method assignment2/Main Method assignment2/MathOperation1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_assignment2
+{
+    class MathOperation1
+    {
+        //Create method that takes two integers as parameters
+        //Make one parameter optional by providing a default value
+        public int MathOperation(int i, int j=1)
+        {
+            //Perform multiplication on integers and return the result
+            int result = i *j;
+            return result;
+        }
+    }
+}
