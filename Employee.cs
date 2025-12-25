4e5ae51ffb7cbb9bc03b7403aba6ed7636ba067a
laura02040 <131707496+laura02040@users.noreply.github.com>
2025-12-25 19:29:53 +0000

    Method And Object
    Method And Object

---
+++ Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MethodsAndObjectsAssignment
+{
+    //Create another class called Employee and have it inherit from the Person class
+    public class Employee : Person
+    {
+            //Give the Employee class a property called Id and have it be of data type int
+            public int Id { get; set; }
+        }
+    }
---
+++ Programm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace MethodsAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inside of the Main method, instantiate and initialize an Employee object with
+            //the first name "Sample" and the last name "Student"
+
+            Employee sam = new Employee() { FirstName = “Sample”, LastName = “Student” };
+            //Call the superclass method SayName() on the Employee object
+
+            sam.SayName();
+
+            Console.ReadLine();
+
+        }
+    }
+}
