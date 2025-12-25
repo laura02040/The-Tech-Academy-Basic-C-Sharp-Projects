317884da9c8828ec44447e5976c2d2998d6e4c8c
laura02040 <131707496+laura02040@users.noreply.github.com>
2025-12-25 15:35:56 +0000

    Method Assesment
    Method Assesment

---
+++ Calculation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Math_Assesment
+{
+    public class Calculation
+    {
+        //method that takes two integers as parameters
+        public int MathOp(int number,int number2)
+        {
+            return number + 7;
+        }
+        public int MathOp(decimal number)
+        {
+            int newnum = Convert.ToInt32(number);
+            return newnum * 20;
+        }
+        //Call the method in the class, passing in two numbers.
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        //Call the method in the class, specifying the parameters by name.
+        static void DisplayInfo(string name, int age, string city)
+        {
+            Console.WriteLine($"Name: {name}, Age: {age}, City: {city}");
+            Console.ReadLine();
+        }
+    }
+
+    }
+
---
+++ Program3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Math_Assesment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                // instantiate the class.
+                Calculation math = new Calculation();
+                //Call the method in the class, passing in two numbers. 
+                Console.WriteLine("Enter first number:");
+                if (!int.TryParse(Console.ReadLine(), out int num1))
+                {
+                    Console.WriteLine("Invalid input. Please enter an integer.");
+                    return;
+                }
+                Console.Write("Enter second number: ");
+                if (!int.TryParse(Console.ReadLine(), out int num2))
+                {
+                    Console.WriteLine("Invalid input. Please enter an integer.");
+                    return;
+                }
+                Console.WriteLine("Input is correct!");
+                Console.ReadLine();
+        }
+    }
+}
