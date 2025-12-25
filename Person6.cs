d4c3d44b3eb00a2af50685041d36008cf68c88e2
laura02040 <131707496+laura02040@users.noreply.github.com>
2025-12-25 21:51:33 +0000

    Abstract classes
    Abstract classes

---
+++ Person6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes_Assignment
+{
+    class Person
+    {
+        //Ctreate an abstract class "Person" with two string properties: firstName and lastName
+        public abstract class Person
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            //Give it the method SayName()
+            public abstract void SayName();
+        }
+    }
+}
---
+++ Program6.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In the Main method, instantiante an Employee object
+            Employee sam = new Employee() { firstName = "Sample", lastName = "Student" };
+            //Call the sayName method on the object
+            sam.SayName();
+            Console.ReadLine();
+        }
+    }
+}
