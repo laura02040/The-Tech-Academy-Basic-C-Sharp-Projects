47dbe393dab9262dbf7fb05aec3d30dda4e7b9b5
laura02040 <131707496+laura02040@users.noreply.github.com>
2025-12-26 17:28:29 +0000

    Operator Assignment
    Operator Assignment0

---
+++ Employee10.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorAssignment
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public static bool operator ==(e1, e2)
+        {
+            if (e1.Id.Equals(e2.Id))
+            {
+                return true;
+            }
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(e1, e2)
+        {
+            if (!e1.Id.Equals(e2.Id))
+            {
+                return true;
+            }
+            {
+                return false;
+            }
+
+        }
+
+    }
+}
---
+++ Program10.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            e1.Id = 1;
+            e1.FirstName = "Bob";
+            e1.LastName = "Jones";
+             Employee e2 = new Employee();
+            e2.Id = 2;
+            e2.FirstName = "Luke";
+            e2.LastName = "Jon";
+            if (e1 == e2)
+            {
+                Console.WriteLine("e1 is equal to e2");
+            }
+            else
+            {
+                Console.WriteLine("e1 is not equal e2");
+            }
+                Console.ReadLine();
+            }
+    }
+}
