7d4dbd7ade54f6ad19213886d3817ca9c5f53f0d
laura02040 <131707496+laura02040@users.noreply.github.com>
2025-12-26 18:09:39 +0000

    Parameters Assignement
    Parameters Assignement0

---
+++ Employee11.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignement
+{
+    //Make the Employee class take a generic type parameter <T>
+    public class Employee<T>
+    {
+        //Add a property called "Things" with a generic list matching the class type
+        public List<T> Things { get; set; }
+        public string Name { get; set; }
+        public Employee(string name)
+        {
+            Name = name;
+            Things = new List<T>();
+        }
+        public void AddThing(T item)
+        {
+            Things.Add(item);
+        }
+    }
+}
