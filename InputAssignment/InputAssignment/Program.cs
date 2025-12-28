fd772632fe7d42e17204b3caa33a22402f75d2c3
laura02040 <131707496+laura02040@users.noreply.github.com>
2025-12-28 22:28:42 +0000

    Input Assignment
    Input

---
+++ InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for a number
+            Console.Write("235 ");
+            string userInput = Console.ReadLine();
+
+            //Logs that number to a text file.
+            Console.WriteLine(userInput);
+            File.WriteAllText("path", txt);
+
+            //Prints the text file back to the user.
+            Console.WriteLine(userInput);
+            using (StreamWriter file=new StreamWriter("path",true))
+            {
+                file.WriteLine(userInput);
+            }
+        }
+    }
+}
