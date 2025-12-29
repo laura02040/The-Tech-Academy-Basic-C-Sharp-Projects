b92ec92513541e84bab04620af23185ca6470349
laura02040 <131707496+laura02040@users.noreply.github.com>
2025-12-29 22:26:17 +0000

    Constructor Assignment
    Constructor assignment

---
+++ ConstructorAssignment/ConstructorAssignment/Chain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class Chain
+    {
+        //Constructor if one parameter, float, is provided
+        public Chain(float num1) : this(num1, "Bob")
+        {
+            Console.WriteLine(num1 + "and your default name is" + Name);
+        }
+        //Constructor if one parameter, a string, is provided
+        public Chain(string name1) : this(42, name1)
+        {
+            Console.WriteLine(name1 + "and your default favorite number is" + Num);
+        }
+        //Constructor if both parameters, a string and float, are provided
+        public Chain(float num, string name)
+        {
+            Name = name;
+            Num = num;
+        }
+        string Name{get; set;}
+        float Num { get; set; }
+    }
+}
