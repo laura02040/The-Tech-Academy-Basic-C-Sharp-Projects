9403afea1d169e69e85fbf158857a2892342ccd7
laura02040 <131707496+laura02040@users.noreply.github.com>
2026-01-04 12:38:49 +0000

    Final Assignment
    Final Assignment

---
+++ Students/Students/Program.cs
@@ -0,0 +1,23 @@
+﻿using Students;
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        using (var context = new StudentContext())
+        {
+            var student = new Student()
+            {
+                StudentName = "John Doe"
+            };
+
+            context.Students.Add(student);
+            context.SaveChanges();
+
+            Console.WriteLine("Student added successfully.");
+        }
+
+        Console.ReadLine();
+    }
+}
